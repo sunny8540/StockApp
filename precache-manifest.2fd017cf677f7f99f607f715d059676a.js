e274d49b27052bc89609dcbb95b19a53267d7d9a
Sunny Raj <111192197+sunny8540@users.noreply.github.com>
2022-12-24 19:44:08 +0530

    Updates

---
+++ precache-manifest.2fd017cf677f7f99f607f715d059676a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1727c3934efebeef4f",
+    "url": "/StockApp/static/css/main.c6e7731c.chunk.css"
+  },
+  {
+    "revision": "0c1727c3934efebeef4f",
+    "url": "/StockApp/static/js/main.0c1727c3.chunk.js"
+  },
+  {
+    "revision": "fed5e64f664a214a437c",
+    "url": "/StockApp/static/js/1.fed5e64f.chunk.js"
+  },
+  {
+    "revision": "9405acac575b52938ccd",
+    "url": "/StockApp/static/js/runtime~main.9405acac.js"
+  },
+  {
+    "revision": "cce9259c330259f28750df673cb198b4",
+    "url": "/StockApp/index.html"
+  }
+];
